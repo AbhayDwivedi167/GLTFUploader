faac12760ea1d8cbea21e47a6c38028f2662f7cc
AbhayDwivedi167 <90592486+AbhayDwivedi167@users.noreply.github.com>
2021-09-14 13:53:51 +0530

    Add files via upload

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,7 @@ export const environment = {
     apiKey: "AIzaSyCL5HrHqSB34fT7sHN4v0TGmnRlynlZFwI",
     authDomain: "yolouploader.firebaseapp.com",
     //databaseURL: "https://exploragram-5dd13-default-rtdb.firebaseio.com",
+	databaseURL: "https://yolouploader-default-rtdb.asia-southeast1.firebasedatabase.app/",
     projectId: "yolouploader",
     storageBucket: "yolouploader.appspot.com",
     messagingSenderId: "1050403599413",
